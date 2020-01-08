a4dec628070ebf810747a6182c2618cf682fadd1
Khoroshikh Arkadiy <khorark@gmail.com>
2020-01-08 18:34:25 +0300

    Added support typescript and input folder

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const svgr = require("@svgr/core").default;
 const SVGO = require("svgo");
 const svg2jsx = require("@balajmarius/svg2jsx");
 const program = require("commander");
-const svgson = require('svgson')
+const svgson = require("svgson");
 
 const { version } = require("./package.json");
 const { config } = require("./configSvgo.js");
@@ -22,14 +22,17 @@ const CONFIG_SVGR = {
 
 program
   .version(version)
+  .option("-i --input [inpath]", "select input folder")
   .option("-o --output [outpath]", "select output folder")
   .option("--expo", "transform to expo format")
+  .option("--ts", "used typescript")
   .option("--suffix", "append suffix icon for out files")
   .on("--help", () => {
     console.log("\nExamples:");
     console.log(`$ svg2rn`);
     console.log(`$ svg2rn -o /home/user/icon`);
     console.log(`$ svg2rn --expo`);
+    console.log(`$ svg2rn --ts`);
     console.log("");
   })
   .parse(process.argv);
@@ -41,12 +44,12 @@ const addAttrsWidthHeightToSvg = async svg => {
   const res = await svgson.parse(svg);
 
   if (res && res.attributes && res.attributes.viewBox) {
-    const viewBox = res.attributes.viewBox.split(' ');
+    const viewBox = res.attributes.viewBox.split(" ");
     res.attributes.width = viewBox[2];
     res.attributes.height = viewBox[3];
   }
 
-  return svgson.stringify(res)
+  return svgson.stringify(res);
 };
 
 // Write new svg file
@@ -55,7 +58,7 @@ const writeSvgFile2Js = async ({
   file,
   componentName,
   pathFileToMin,
-  timeStart,
+  timeStart
 }) => {
   // Config SVGO
   let svgo = new SVGO(config);
@@ -72,7 +75,7 @@ const writeSvgFile2Js = async ({
       },
       {
         removeViewBox: false
-      },
+      }
     ]
   });
   svgOptimize = await svgo.optimize(svgClassToStyleAttrs);
@@ -84,6 +87,11 @@ const writeSvgFile2Js = async ({
   // Check support expo
   if (program.expo) {
     result = trasformToExpo(result);
+  } 
+  
+  // Check support typescript
+  if (program.ts) {
+    result = transformToTs(result);
   }
 
   // Write file
@@ -92,41 +100,59 @@ const writeSvgFile2Js = async ({
     else {
       const timeFinish = parseInt(performance.now() - timeStart);
       console.log("");
-      console.log(`${file}: => ${componentName}.js`);
+      console.log(`${file} => ${componentName}.${program.ts ? "tsx" : "js"}`);
       console.log(`Done in ${timeFinish} ms!`);
     }
   });
 };
 
 const trasformToExpo = jsx => {
-  const jsxArr = jsx.split(';\n');
-  jsxArr[1] = `import { Svg } from 'expo'`
-  let data = jsxArr.join(';\n');
+  const jsxArr = jsx.split(";\n");
+  jsxArr[1] = `import { Svg } from 'expo'`;
+  let data = jsxArr.join(";\n");
 
   data = data.replace(/<(\w+)/g, str => {
-    let newStr = '';
-    if (str !== '<Svg') {
-      newStr = str.replace('<', '<Svg.')
+    let newStr = "";
+    if (str !== "<Svg") {
+      newStr = str.replace("<", "<Svg.");
     } else {
       newStr = str;
     }
 
     return newStr;
-  })
-  
+  });
+
   return data.replace(/<\/(\w+)/g, str => {
-    let newStr = '';
-    if (str !== '</Svg') {
-      newStr = str.replace('</', '</Svg.')
+    let newStr = "";
+    if (str !== "</Svg") {
+      newStr = str.replace("</", "</Svg.");
     } else {
       newStr = str;
     }
 
     return newStr;
-  })
+  });
+};
+
+const transformToTs = jsx => {
+  const regexpImportPropsWithExt = / } from/g;
+  const regexpProps = /= props =/;
+  const isExpo = program.expo;
+
+  if (isExpo) {
+    return jsx.replace(regexpProps, "= (props: any) =");
+  } else {
+    const res = regexpImportPropsWithExt.exec(jsx);
+    if (!res[0]) return jsx;
+  
+    const data = jsx.slice(0, res.index) + ", SvgProps" + jsx.slice(res.index);
+  
+    return data.replace(regexpProps, "= (props: SvgProps) =");
+  }
 };
 
 const main = () => {
+  const inPath = program.input || __dirname;
   const outPath = program.output || PATH_TO_COMPONENT_DIR;
   const haveSuffix = !!program.suffix;
 
@@ -134,23 +160,27 @@ const main = () => {
   if (!fs.existsSync(outPath)) fs.mkdirSync(outPath);
 
   // Read all files from dir
-  const files = fs.readdirSync("./");
+  const files = fs.readdirSync(inPath);
 
   files.forEach(file => {
     if (path.extname(file) !== ".svg") return false;
     const timeStart = performance.now();
 
     // Work with files
-    const svg = fs.readFileSync(`./${file}`, { encoding: "utf-8" });
-    const componentName = `${capitalize(path.basename(file, ".svg").trim())}${haveSuffix ? 'Icon' : ''}`;
-    const pathFileToMin = `${outPath}/${componentName}.js`;
+    const svg = fs.readFileSync(`${inPath}/${file}`, { encoding: "utf-8" });
+    const componentName = `${capitalize(path.basename(file, ".svg").trim())}${
+      haveSuffix ? "Icon" : ""
+    }`;
+    const pathFileToMin = `${outPath}/${componentName}.${
+      program.ts ? "tsx" : "js"
+    }`;
 
     const payload = {
       svg,
       file,
       componentName,
       pathFileToMin,
-      timeStart,
+      timeStart
     };
 
     writeSvgFile2Js(payload);
